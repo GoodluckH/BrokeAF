ce3a584c024d311ba5c0d3c865ae6c804e6fa3ad
Xipu Li <lixipu98@gmail.com>
2023-02-17 10:44:48 -0800

    fix: hide menu and hosted by streamlit

--- utils.py
+++ utils.py
@@ -149,8 +149,8 @@ def set_page_config():
     # hide hosted by Streamlit button
     hide_streamlit_style = """
         <style>
-        MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
         </style>
         """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
